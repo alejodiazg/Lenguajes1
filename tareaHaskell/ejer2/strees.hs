3274b16f0c49581ec465f59c53cc51f8aea3fefc
alejodiazg <09-10234@usb.ve>
2016-10-16 20:00:28 -0400

    agrego lo que hicimos el jueves

--- tareaHaskell/ejer2/strees.hs
+++ tareaHaskell/ejer2/strees.hs
@@ -8,10 +8,10 @@ isPrefix _ []           = False
 isPrefix (x:xs) (y:ys)  = x == y && isPrefix xs ys
 
 removePrefix :: String -> String -> String
-removePrefix [] ys = ys
+removePrefix [] ys                    = ys
 removePrefix (x:xs) (y:ys)
-  | x == y = removePrefix xs ys
-removePrefix _ ys = ys
+  | x == y  && isPrefix (x:xs) (y:ys) = removePrefix xs ys
+removePrefix _ ys                     = ys
 
 suffixes :: [a] -> [[a]]
 suffixes [] = []
@@ -22,3 +22,23 @@ isSubstring xs ys = foldr (f xs) False (suffixes ys)
   where
     f as bs = (||) (isPrefix as bs)
 
+findSubstrings :: String -> String -> [Int]
+findSubstrings [] _  = []
+findSubstrings xs ys = f xs ys 0
+	where
+		f _ [] _= []
+		f xs ys n
+   		  | (isPrefix xs ys) = n : f xs (tail ys) (n+1)
+		f xs ys n = f xs (tail ys) (n+1)
+
+--f :: String -> String -> Int -> [Int]
+--f _ [] _ = []
+--f xs ys n
+--   | (isPrefix xs ys) = n : f xs (tail ys) (n+1)
+--f xs ys n = f xs (tail ys) (n+1)
+
+getIndices :: SuffixTree -> [Int]
+getIndices (Leaf n) = [n]
+getIndices (Node ((s,t):ys)) 
+   | ys == [] = getIndices(t)
+getIndices (Node ((s,t):ys)) = getIndices(t) ++ getIndices(Node ys)
